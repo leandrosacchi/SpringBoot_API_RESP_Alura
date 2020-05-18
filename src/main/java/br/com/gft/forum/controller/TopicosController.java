eb3efeec4bc75fe5f6cdb27ad414201966a51c19
leandro-gft <lpsacchi@gmail.com>
2020-05-18 13:42:35 -0300

    Implementação de DTO

--- src/main/java/br/com/gft/forum/controller/TopicosController.java
+++ src/main/java/br/com/gft/forum/controller/TopicosController.java
@@ -3,24 +3,24 @@ package br.com.gft.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.gft.forum.controller.dto.TopicoDto;
 import br.com.gft.forum.model.Curso;
 import br.com.gft.forum.model.Topico;
 
-@Controller
+@RestController
 @RequestMapping
 public class TopicosController {
 
 	@GetMapping(value = "/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
-		Topico topico1 = new Topico("Duvida", "Duvida com Spring", new Curso("Spring Boot", "Programação"));
-		
-		return Arrays.asList(topico1, topico1, topico1);
+	public ResponseEntity<List<TopicoDto>> lista() {
+		Topico topico1 = new Topico("Dúvida", "Duvida com Spring", new Curso("Spring Boot", "Programação"));
+		List<Topico> lista = Arrays.asList(topico1, topico1, topico1);
+		return ResponseEntity.ok(TopicoDto.converter(lista));
 		
 	}
 }
---
+++ src/main/java/br/com/gft/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,48 @@
+package br.com.gft.forum.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+
+import br.com.gft.forum.model.Topico;
+
+public class TopicoDto {
+	
+	private Long id;
+	private String titulo;
+	private String msg;
+	private LocalDateTime dataCriacao;
+	 
+	public TopicoDto(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.msg = topico.getMensagem();
+		this.dataCriacao = topico.getDataCriacao();
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+
+	public static List<TopicoDto> converter(List<Topico> topicos) {
+		
+		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+	}
+	
+	
+	
+}
