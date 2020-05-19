56e18967987185b124362f3c695adcac9d947eba
leandro-gft <lpsacchi@gmail.com>
2020-05-19 12:49:19 -0300

    Simplificação do tratamento de erros

---
+++ src/main/java/br/com/gft/forum/config/validacao/ErroFormDto.java
@@ -0,0 +1,34 @@
+package br.com.gft.forum.config.validacao;
+
+public class ErroFormDto {
+	
+	private String campo;
+	private String erro;
+	
+	public ErroFormDto() {
+		
+	}
+
+	public ErroFormDto(String campo, String erro) {
+		this.campo = campo;
+		this.erro = erro;
+	}
+
+	public String getCampo() {
+		return campo;
+	}
+
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+
+	public String getErro() {
+		return erro;
+	}
+
+	public void setErro(String erro) {
+		this.erro = erro;
+	}
+
+	
+}
